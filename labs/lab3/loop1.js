ecc63fa7b1bcbdd31cf059195410d7c39f79062a
pvarney13 <pvarney@iu.edu>
2021-09-15 23:17:47 -0400

    creating lab 3

---
+++ labs/lab3/loop1.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(400,400)
+}
+
+function draw(){
+    background(243,15,132)
+    stroke(66, 245, 93)
+    noFill()
+
+    for (let y = 0; y < 15; y++) {
+        circle(200, 200, y * 30)
+    
+    }
+    
+}
